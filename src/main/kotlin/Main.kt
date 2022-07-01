023941be0664469b1afa36b06681d83b74c1cc64
MidNight <levanalavidze@hotmail.com>
2022-07-01 14:23:11 +0400

    minSplit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main(){
+
+    val x = minSplit()
+
+    println("coins $x")
+
+
+
+}
+
+fun minSplit(): Int {
+
+    //set amount
+    val geoTetri = 104
+
+    var change = 0
+    val a = geoTetri / 50
+    change = geoTetri % 50
+    val b = change / 20
+    change %= 20
+    val c = change / 10
+    change %= 10
+    val d = change / 5
+    change %= 5
+    val e = change/1
+    change%= 1
+
+    return a + b + c + d + e
+
+}
+
+
